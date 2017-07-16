7fde089515c42d838d9fd4af6c7e5cdd22897005
Adam Palethorpe <adam.palethorpe@supergroup.co.uk>
2017-07-16 17:51:02 +0100

    Refactoring, updated .env.example, added support for TV episodes

--- app/Services/KodiService.php
+++ app/Services/KodiService.php
@@ -43,60 +43,61 @@ class KodiService
 	public function playMovie(AlexaRequest $request)
 	{
 		$requestedMovie = $request->getValue('MovieTitle');
+		$selectedMovie = $this->getClosestMatchingTitle($this->getMovies(), $requestedMovie);
 
-		if (!$requestedMovie) {
-			throw new \Exception('No movie title provided');
-		}
-
-		$movies = $this->getMovies();
-		$selectedMovie = null;
-
-		// Try to find an exact match or a title containing every word in the request
-		$requestedMovieParts = explode(' ', $requestedMovie);
-
-		foreach ($movies['result']['movies'] as $movie) {
-			if (strtolower($movie['label']) == strtolower($requestedMovie)) {
-				$selectedMovie = $movie;
-				break;
-			}
-
-			$score = 0;
-
-			foreach ($requestedMovieParts as $requestedMoviePart) {
-				if (stripos(preg_replace('/[A-Za-z0-9\s]/', '', $movie['label']), $requestedMoviePart) !== false) {
-					$score++;
-				}
-			}
-
-			if ($score == count($requestedMovieParts)) {
-				$selectedMovie = $movie;
-			}
-		}
-
-		// If nothing is found, try to find the closest title
-		if (!$selectedMovie) {
-			$bestScore = null;
-
-			foreach ($movies['result']['movies'] as $movie) {
-				$score = levenshtein($requestedMovie, $movie['label']);
-				if ($bestScore === null || $bestScore > $score) {
-					$bestScore = $score;
-					$selectedMovie = $movie;
-				}
-			}
-		}
-
-		$params = ['method' => 'Player.Open', 'params' => ['item' => ['movieid' => $selectedMovie['movieid']]]];
-
-		$this->curl->get($params);
-
-		if ($this->curl->getLastStatusCode() == 200) {
+		if ($this->play($selectedMovie['movieid'])) {
 			$responseText = sprintf('Playing movie %s', $selectedMovie['label']);
 		} else {
 			$responseText = 'Something went wrong';
 		}
 
 		return $responseText;
+	}
+
+	public function playTVShow(AlexaRequest $request)
+	{
+		$requestedShow = $request->getValue('TVShowTitle');
+		$selectedShow = $this->getClosestMatchingTitle($this->getTVShows(), $requestedShow);
+
+		$requestedSeason = $request->getValue('Season');
+		$requestedEpisode = $request->getValue('Episode');
+
+		$episodes = $this->getEpisodes($selectedShow['tvshowid']);
+
+		$responseText = null;
+
+		foreach ($episodes as $episode) {
+			$isCorrectSeason = (!$requestedSeason || ($requestedSeason && $episode['season'] == $requestedSeason));
+			$isCorrectEpisode = $requestedEpisode == $episode['episode'];
+
+			if ($isCorrectSeason && $isCorrectEpisode) {
+				if ($this->play($episode['episodeid'], 'episode')) {
+					$responseText = sprintf('Playing %s', $requestedShow);
+				} else {
+					$responseText = 'Something went wrong';
+				}
+			}
+		}
+
+		if (!$responseText) {
+			$responseText = sprintf(
+				'I could\'nt find %s %s episode %d',
+				$requestedShow,
+				$requestedSeason ? sprintf('season %d', $requestedSeason) : '',
+				$requestedEpisode
+			);
+		}
+
+		return $responseText;
+	}
+
+	private function play($itemId, $type = 'movie')
+	{
+		$params = ['method' => 'Player.Open', 'params' => ['item' => [$type . 'id' => $itemId]]];
+		
+		$this->curl->get($params);
+
+		return $this->curl->getLastStatusCode() == 200;
 	}
 
 	private function getPlayer()
@@ -134,6 +135,67 @@ class KodiService
 	private function getMovies()
 	{
 		$params = ['method' => 'VideoLibrary.GetMovies'];
-		return $this->curl->get($params);
+		return $this->curl->get($params)['result']['movies'];
+	}
+
+	private function getTVShows()
+	{
+		$params = ['method' => 'VideoLibrary.GetTVShows'];
+		return $this->curl->get($params)['result']['tvshows'];
+	}
+
+	private function getEpisodes($tvShowId)
+	{
+		$params = [
+			'method' => 'VideoLibrary.GetEpisodes',
+			'params' => [
+				'tvshowid' => $tvShowId,
+				'properties' => ['season', 'episode']
+			]
+		];
+		
+		return $this->curl->get($params)['result']['episodes'];
+	}
+
+	private function getClosestMatchingTitle($items, $target)
+	{
+		$selected = null;
+
+		// Try to find an exact match or a title containing every word in the request
+		$targetParts = explode(' ', $target);
+
+		foreach ($items as $item) {
+			if (strtolower($item['label']) == strtolower($target)) {
+				$selected = $item;
+				break;
+			}
+
+			$score = 0;
+
+			foreach ($targetParts as $targetPart) {
+				if (stripos(preg_replace('/[A-Za-z0-9\s]/', '', $item['label']), $targetPart) !== false) {
+					$score++;
+				}
+			}
+
+			if ($score == count($targetParts)) {
+				$selected = $item;
+			}
+		}
+
+		// If nothing is found, try to find the closest title
+		if (!$selected) {
+			$bestScore = null;
+
+			foreach ($items as $item) {
+				$score = levenshtein($target, $item['label']);
+				if ($bestScore === null || $bestScore > $score) {
+					$bestScore = $score;
+					$selected = $item;
+				}
+			}
+		}
+
+		return $selected;
 	}
 }
